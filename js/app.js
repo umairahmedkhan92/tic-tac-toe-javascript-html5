a189711193a62c75327ca874b41efd125ee368a8
umairahmedkhan92 <umairahmedkhan1992@gmail.com>
2016-11-23 03:28:00 +0500

    js update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
- * Javascript for Tic Tac Toe Game
- * Author: Umair Ahmed Khan
- */
+* Javascript for Tic Tac Toe Game
+* Author: Umair Ahmed Khan
+*/
 
 /* Initilizatiion of properties */
 var thisTile, tileCanvas, context;
@@ -14,35 +14,35 @@ var offset = 15;
 
 /* Draws the tic tac toe board. */
 function drawCanvas(){
-  var container = document.getElementById("container");
+    var container = document.getElementById("container");
 
     /* Function reference for tile onclick event */
-   	var tileClickEvent = function(index){
-      return function(){
-        tileClicked(index);
-      };
+    var tileClickEvent = function(index){
+        return function(){
+            tileClicked(index);
+        };
     };
 
-     /* Creates empty tiles */
-  	for (var i = 0; i < board; i ++){
-  		filled[i] = false;
-	    var tile = document.createElement("canvas");
-	    tile.height = tile.width = tileSize;
-	    tile.id = "tile_" + i;
-	    tile.onclick = tileClickEvent(i);
+    /* Creates empty tiles */
+    for (var i = 0; i < board; i ++){
+        filled[i] = false;
+        var tile = document.createElement("canvas");
+        tile.height = tile.width = tileSize;
+        tile.id = "tile_" + i;
+        tile.onclick = tileClickEvent(i);
 
-	    container.appendChild(tile);
-	    if (i == 2 || i == 5){
-	      	var br = document.createElement("br");
-	      	container.appendChild(br);
-	    }
-  	}
+        container.appendChild(tile);
+        if (i == 2 || i == 5){
+            var br = document.createElement("br");
+            container.appendChild(br);
+        }
+    }
 }
 
 /**
- * Called when a tile is clicked on the board.
- * @param {number} tileNumber - ID of the tile selected.
- */
+* Called when a tile is clicked on the board.
+* @param {number} tileNumber - ID of the tile selected.
+*/
 function tileClicked(tileNumber){
     thisTile = "tile_"+tileNumber;
     tileCanvas = document.getElementById(thisTile);
@@ -54,15 +54,15 @@ function tileClicked(tileNumber){
     /* Draws a shape if the tile if empty */
     if(filled[tileNumber] == false){
         if(turn%2==0){  /* Draws X */
-        	context.strokeStyle = "#f1be32";
+            context.strokeStyle = "#f1be32";
             context.moveTo(offset,offset);
             context.lineTo(tileSize-offset, tileSize-offset);
             context.moveTo(tileSize-offset, offset);
             context.lineTo(offset, tileSize-offset);
         }
         else{ /* Draws O */
-        	context.strokeStyle = "#01bBC2";
-          	context.arc(tileSize/2, tileSize/2, (tileSize/2) - offset,0,Math.PI*2,true);
+            context.strokeStyle = "#01bBC2";
+            context.arc(tileSize/2, tileSize/2, (tileSize/2) - offset,0,Math.PI*2,true);
         }
         context.stroke();
         context.closePath();
@@ -91,23 +91,23 @@ function gameOver(){
 
 /* Clears the borad for a new game */
 function clearTiles(){
-	for (var i = 0; i < board; i++) {
-		thisTile = "tile_"+i;
-    	tileCanvas = document.getElementById(thisTile);
-    	context = tileCanvas.getContext("2d");
-		context.clearRect(0, 0, tileCanvas.width, tileCanvas.height);
-		filled[i] =  false; 
-		tilesFilled = 0;
+    for (var i = 0; i < board; i++) {
+        thisTile = "tile_"+i;
+        tileCanvas = document.getElementById(thisTile);
+        context = tileCanvas.getContext("2d");
+        context.clearRect(0, 0, tileCanvas.width, tileCanvas.height);
+        filled[i] =  false; 
+        tilesFilled = 0;
         turn = 0;
-	}
+    }
 }
 
 /* Displays the game screen */
 function startGame(){
-	document.getElementById("start").className = 'hide';
-	document.getElementById("game").className = "animated fadeInRight";
+    document.getElementById("start").className = 'hide';
+    document.getElementById("game").className = "animated fadeInRight";
     drawCanvas();
-	myShakeEvent.start();
+    myShakeEvent.start();
 }
 
 /* Triggered on the shake event */
@@ -117,7 +117,7 @@ function shakeEventDidOccur () {
 
 /* Initilization of shake event */
 var myShakeEvent = new Shake({
-	threshold: 5
+    threshold: 5
 });
 
 /* Shake event listener */
